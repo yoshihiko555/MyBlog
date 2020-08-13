f854da3614e6b902b9316afe880371ec140584cb
yoshihiko <nkty.yoshihiko@gmail.com>
2020-08-14 01:28:16 +0900

    dockerfile修正5

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,7 +9,7 @@ env.read_env('.env')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
