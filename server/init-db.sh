025753f40e7c16ff154733d448a2b78d31c5348c
yoshihiko555 <yoshihiko05410@gmail.com>
2021-05-07 19:53:17 +0900

    commit1

---
+++ server/init-db.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# DBファイルの削除
+rm -rf ./api/migrations
+rm -rf db.sqlite3
+
+# マイグレーション
+# read -p "Please enter the app name:" app_name
+# python manage.py makemigrations $app_name
+python manage.py makemigrations api
+
+# マイグレート
+python manage.py migrate
+
+# admin作成
+python manage.py create_admin
+
+# サーバー起動
+python manage.py runserver 0.0.0.0:8000
